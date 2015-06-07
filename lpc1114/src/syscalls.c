e9958d5a73bee979e0f3b95f040f1a94d9999296
lix <lix@imac.local>
2015-06-07 23:12:36 +0200

    New stubs added; support for malloc (_sbrk).

--- lpc1114/src/syscalls.c
+++ lpc1114/src/syscalls.c
@@ -1,31 +1,52 @@
 /*
  * syscalls.c
  *
- * (c) 2015 Lixco Microsystems <lix@paulian.net>
- *
  * Created on: Jun 4, 2015 (LNP)
+ *
+ * Copyright (c) 2011-2015 Lixco Microsystems <lix@paulian.net>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
  */
 
+/* A minimum implementation of the stubs required by newlib */
 
 #include <sys/types.h>
 #include <errno.h>
+#include <sys/stat.h>
 #include "FreeRTOS.h"
 #include "task.h"
 #include "olimex_p1114.h"
 
+/* initial heap size, it will be properly set after the first malloc() */
+int free_heap = 8192 - 1024;
+
 
 caddr_t _sbrk(int incr)
 {
-	extern char _pvHeapStart; /* see linker definition */
+	extern char _pvHeapStart; /* see linker definitions */
 	extern char _vStackTop;
 
 	static char* current_heap_end;
 	char* current_block_address;
 
 	if (current_heap_end == 0)
-	{
 		current_heap_end = &_pvHeapStart;
-	}
 
 	current_block_address = current_heap_end;
 
@@ -39,6 +60,7 @@ caddr_t _sbrk(int incr)
 	}
 
 	current_heap_end += incr;
+	free_heap = &_vStackTop - current_heap_end;
 
 	return (caddr_t) current_block_address;
 }
@@ -80,5 +102,35 @@ int _read (int file, char *ptr, int len)
 		break;
 	}
 	return n;
+}
+
+int __attribute__((weak))
+_lseek(int file __attribute__((unused)), int ptr __attribute__((unused)),
+    int dir __attribute__((unused)))
+{
+  errno = ENOSYS;
+  return -1;
+}
+
+int __attribute__((weak))
+_fstat(int fildes __attribute__((unused)),
+    struct stat* st __attribute__((unused)))
+{
+  errno = ENOSYS;
+  return -1;
+}
+
+int __attribute__((weak))
+_close(int fildes __attribute__((unused)))
+{
+  errno = ENOSYS;
+  return -1;
+}
+
+int __attribute__((weak))
+_isatty(int file __attribute__((unused)))
+{
+  errno = ENOSYS;
+  return 0;
 }
 
